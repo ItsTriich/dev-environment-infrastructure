2745fc019f1076e8c6db49090adb968bd3191024
Travis Gray <Travis.Gray@okaloosaschools.com>
2024-03-28 13:38:45 -0500

    updated the code

--- main.tf
+++ main.tf
@@ -98,3 +98,31 @@ resource "azurerm_network_interface" "mtc_nic" {
     environment = "dev"
   }
 }
+
+resource "azurerm_linux_virtual_machine" "mtc_vm" {
+  name                = "mtc-vm"
+  resource_group_name = azurerm_resource_group.mtc_rg.name
+  location            = azurerm_resource_group.mtc_rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.mtc_nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/mtcazurekey.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}

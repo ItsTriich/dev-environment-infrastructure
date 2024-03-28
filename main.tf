24dac278ef6aa05bbcfaa8f78cb767c352d695bd
Travis Gray <Travis.Gray@okaloosaschools.com>
2024-03-28 12:16:56 -0500

    added public_ip and network interface

---
+++ main.tf
@@ -0,0 +1,100 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0" # Use pessimistic version constraint for compatibility
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "mtc_rg" {
+  name     = "mtc-resources"
+  location = "East US"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Creating a Virtual network
+resource "azurerm_virtual_network" "mtc_vn" {
+  name                = "mtc-network"
+  resource_group_name = azurerm_resource_group.mtc_rg.name
+  location            = azurerm_resource_group.mtc_rg.location
+  address_space       = ["10.123.0.0/16"]
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Subnet
+resource "azurerm_subnet" "mtc_subnet" {
+  name                 = "mtc-subnet"
+  resource_group_name  = azurerm_resource_group.mtc_rg.name
+  virtual_network_name = azurerm_virtual_network.mtc_vn.name
+  address_prefixes     = ["10.123.1.0/24"]
+}
+
+# Network Security Group
+resource "azurerm_network_security_group" "mtc_sg" {
+  name                = "mtc-sg"
+  location            = azurerm_resource_group.mtc_rg.location
+  resource_group_name = azurerm_resource_group.mtc_rg.name
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "mtc_dev_rule" {
+  name                        = "mtc-dev-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.mtc_rg.name
+  network_security_group_name = azurerm_network_security_group.mtc_sg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "mtc_sga" {
+  subnet_id                 = azurerm_subnet.mtc_subnet.id
+  network_security_group_id = azurerm_network_security_group.mtc_sg.id
+}
+
+resource "azurerm_public_ip" "mtc_ip" {
+  name                = "mtc-ip"
+  resource_group_name = azurerm_resource_group.mtc_rg.name
+  location            = azurerm_resource_group.mtc_rg.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_interface" "mtc_nic" {
+  name                = "mtc-nic"
+  location            = azurerm_resource_group.mtc_rg.location
+  resource_group_name = azurerm_resource_group.mtc_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.mtc_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.mtc_ip.id
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}

fe98906df522782b7cbe5914ac850e8bd8eb6bf2
Lil Took6🥀 <travusgray@Lils-MacBook-Pro.local>
2024-03-28 18:24:56 -0500

    updated

--- main.tf
+++ main.tf
@@ -11,48 +11,59 @@ provider "azurerm" {
   features {}
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "mtc_rg" {
+# Variables
+
+variable "location" {
+  default = "East US"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+# Resource Group
+resource "azurerm_resource_group" "mtc-rg" {
   name     = "mtc-resources"
-  location = "East US"
+  location = var.location
 
   tags = {
-    environment = "dev"
+    environment = var.environment
   }
 }
 
-# Creating a Virtual network
-resource "azurerm_virtual_network" "mtc_vn" {
+# Virtual Network
+resource "azurerm_virtual_network" "mtc-vn" {
   name                = "mtc-network"
-  resource_group_name = azurerm_resource_group.mtc_rg.name
-  location            = azurerm_resource_group.mtc_rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
   address_space       = ["10.123.0.0/16"]
 
   tags = {
-    environment = "dev"
+    environment = var.environment
   }
 }
 
 # Subnet
-resource "azurerm_subnet" "mtc_subnet" {
+resource "azurerm_subnet" "mtc-subnet" {
   name                 = "mtc-subnet"
-  resource_group_name  = azurerm_resource_group.mtc_rg.name
-  virtual_network_name = azurerm_virtual_network.mtc_vn.name
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
+  virtual_network_name = azurerm_virtual_network.mtc-vn.name
   address_prefixes     = ["10.123.1.0/24"]
 }
 
 # Network Security Group
-resource "azurerm_network_security_group" "mtc_sg" {
+resource "azurerm_network_security_group" "mtc-sg" {
   name                = "mtc-sg"
-  location            = azurerm_resource_group.mtc_rg.location
-  resource_group_name = azurerm_resource_group.mtc_rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
 
   tags = {
-    environment = "dev"
+    environment = var.environment
   }
 }
 
-resource "azurerm_network_security_rule" "mtc_dev_rule" {
+# Network Security Rule
+resource "azurerm_network_security_rule" "mtc-dev-rule" {
   name                        = "mtc-dev-rule"
   priority                    = 100
   direction                   = "Inbound"
@@ -60,58 +71,62 @@ resource "azurerm_network_security_rule" "mtc_dev_rule" {
   protocol                    = "*"
   source_port_range           = "*"
   destination_port_range      = "*"
-  source_address_prefix       = "*"
+  source_address_prefix       = "98.170.249.211/32"
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.mtc_rg.name
-  network_security_group_name = azurerm_network_security_group.mtc_sg.name
+  resource_group_name         = azurerm_resource_group.mtc-rg.name
+  network_security_group_name = azurerm_network_security_group.mtc-sg.name
 }
 
-resource "azurerm_subnet_network_security_group_association" "mtc_sga" {
-  subnet_id                 = azurerm_subnet.mtc_subnet.id
-  network_security_group_id = azurerm_network_security_group.mtc_sg.id
+# Subnet NSG Association
+resource "azurerm_subnet_network_security_group_association" "mtc-sga" {
+  subnet_id                 = azurerm_subnet.mtc-subnet.id
+  network_security_group_id = azurerm_network_security_group.mtc-sg.id
 }
 
-resource "azurerm_public_ip" "mtc_ip" {
+# Public IP
+resource "azurerm_public_ip" "mtc-ip" {
   name                = "mtc-ip"
-  resource_group_name = azurerm_resource_group.mtc_rg.name
-  location            = azurerm_resource_group.mtc_rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
   allocation_method   = "Dynamic"
 
   tags = {
-    environment = "dev"
+    environment = var.environment
   }
 }
 
-resource "azurerm_network_interface" "mtc_nic" {
+# Network Interface
+resource "azurerm_network_interface" "mtc-nic" {
   name                = "mtc-nic"
-  location            = azurerm_resource_group.mtc_rg.location
-  resource_group_name = azurerm_resource_group.mtc_rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.mtc_subnet.id
+    subnet_id                     = azurerm_subnet.mtc-subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.mtc_ip.id
+    public_ip_address_id          = azurerm_public_ip.mtc-ip.id
   }
 
   tags = {
-    environment = "dev"
+    environment = var.environment
   }
 }
 
-resource "azurerm_linux_virtual_machine" "mtc_vm" {
+# Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "mtc-vm" {
   name                = "mtc-vm"
-  resource_group_name = azurerm_resource_group.mtc_rg.name
-  location            = azurerm_resource_group.mtc_rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
   size                = "Standard_B1s"
   admin_username      = "adminuser"
   network_interface_ids = [
-    azurerm_network_interface.mtc_nic.id
+    azurerm_network_interface.mtc-nic.id
   ]
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/mtcazurekey.pub")
+    public_key = file("/Users/travusgray/Documents/Confidential/mtcazurekey.pub")
   }
 
   os_disk {
